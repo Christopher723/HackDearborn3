cd32a98d800bed4244011e7285201f2a71c61fcf
Christopher <Christopher.woods723@gmail.com>
2024-10-12 22:48:49 -0400

    cleaned up

--- Split/View/Main/StartView.swift
+++ Split/View/Main/StartView.swift
@@ -39,26 +39,11 @@ struct StartView: View {
         } else {
             
             let formDetails = FormDetailsView(names: $names, newUserName: $newUserName, currencyType: $currencyType, showAlert1: $showAlert1, showAlert2: $showAlert2)
-            
             NavigationView {
                 VStack{
-                    NavigationLink(destination: HistoryView(showHistoryView: $showHistoryView), isActive: $showHistoryView) { EmptyView() }
-                        .isDetailLink(false)
-                        .navigationViewStyle(.stack)
-                    
-                    Form {
-                        Section {
-                            EmptyView()
-                            //to fix this bug: https://www.hackingwithswift.com/forums/swiftui/issues-with-list-section-headers-in-ios-15/9891
-                        }
-                        
-                        Section {
-                            FillFavoriteButton(names: $names, newUserName: $newUserName, currencyType: $currencyType, update: $showFavoriteView)
-                            EditFavoriteButton(showFavoriteView: $showFavoriteView)
-                        } header: {
-                            //Text("Favorites")
-                        }
-                        
+              Form {
+                       
+
                         
                         formDetails
                         
@@ -69,110 +54,6 @@ struct StartView: View {
                         Spacer()
                         
                         Group {
-                            Button {
-                                let ok = formDetails.isFinalUsersCorrect()
-                                if ok {
-                                    showResults = false
-                                    ParametersStore.load { result in
-                                        switch result {
-                                        case .failure(let error):
-                                            fatalError(error.localizedDescription)
-                                        case .success(let parameters):
-                                            model.parameters = parameters
-                                        }
-                                    }
-                                    for name in names{
-                                        model.users.append(User(name: name))
-                                    }
-                                    model.currency = Currency(symbol: currencyType)
-                                    withAnimation() {
-                                        model.photoIsImported = true
-                                        model.startTheProcess = true
-                                        filesSheet = true
-                                    }
-                                }
-                            } label: {
-                                StartCustomButtons(role: "files")
-                            }
-                            .fileImporter(
-                                isPresented: $filesSheet,
-                                allowedContentTypes: [.image],
-                                allowsMultipleSelection: true
-                            ) { result in
-                                do {
-                                    var selectedImages: [UIImage] = []
-                                    let selectedFiles: [URL] = try result.get()
-                                    for selectedFile in selectedFiles {
-                                        guard selectedFile.startAccessingSecurityScopedResource() else { return }
-                                        
-                                        if let imageData = try? Data(contentsOf: selectedFile),
-                                           let image = UIImage(data: imageData) {
-                                            selectedImages.append(image)
-                                        }
-                                        
-                                        selectedFile.stopAccessingSecurityScopedResource()
-                                    }
-//                                    self.selectedImages = selectedImages
-                                    
-                                    let textRecognitionFunctions = TextRecognitionFunctions(model: model, recognizedContent: recognizedContent)
-                                    textRecognitionFunctions.fillInModel(images: selectedImages) { nothing in
-                                        nothingFound = nothing
-                                    }
-                                    withAnimation {
-                                        showResults = true
-                                        filesSheet = false
-                                    }
-                                } catch {
-                                    Swift.print(error.localizedDescription)
-                                }
-                            }
-                            .onChange(of: filesSheet, perform: { newValue in
-                                if !newValue && !showResults { // sheet dismissed because the user has cancelled
-                                    withAnimation() {
-                                        model.eraseModelData(eraseScanFails: false, fast: true)
-                                    }
-                                }
-                            })
-                            
-                            Button {
-                                let ok = formDetails.isFinalUsersCorrect()
-                                if ok {
-                                    showResults = false
-                                    ParametersStore.load { result in
-                                        switch result {
-                                        case .failure(let error):
-                                            fatalError(error.localizedDescription)
-                                        case .success(let parameters):
-                                            model.parameters = parameters
-                                        }
-                                    }
-                                    for name in names{
-                                        model.users.append(User(name: name))
-                                    }
-                                    model.currency = Currency(symbol: currencyType)
-                                    withAnimation() {
-                                        model.photoIsImported = true
-                                        model.startTheProcess = true
-                                        photoLibrarySheet = true
-                                    }
-                                }
-                            } label: {
-                                StartCustomButtons(role: "library")
-                            }
-                            .sheet(isPresented: $photoLibrarySheet, onDismiss: {
-                                if !showResults { // sheet dismissed because the user has cancelled
-                                    withAnimation() {
-                                        model.eraseModelData(eraseScanFails: false, fast: true)
-                                    }
-                                }
-                            }){
-                                ShowLibraryView(nothingFound: $nothingFound, showResults: $showResults, showSheet: $photoLibrarySheet)
-                            }
-
-                            if horizontalSizeClass == .compact { // Only appears on iPhone
-                                Spacer()
-                            }
-                            
                             Button {
                                 let ok = formDetails.isFinalUsersCorrect()
                                 if ok {
@@ -212,34 +93,7 @@ struct StartView: View {
                     }
                     
                     ToolbarItemGroup(placement: .navigationBarTrailing){
-                        Button {
-                            ParametersStore.load { result in
-                                switch result {
-                                case .failure(let error):
-                                    fatalError(error.localizedDescription)
-                                case .success(let parameters):
-                                    model.parameters = parameters
-                                }
-                            }
-                            withAnimation() {
-                                showHistoryView = true
-                            }
-                        } label: {
-                            Image(systemName: "clock.arrow.circlepath")
-                                .frame(height: 96, alignment: .trailing) // because of https://stackoverflow.com/questions/58512344/swiftui-navigation-bar-button-not-clickable-after-sheet-has-been-presented
-                        }
-                        
-                        Button {
-                            showSettings = true
-                        } label: {
-                            Image(systemName: "gear")
-                                .frame(height: 96, alignment: .trailing)
-                        }
-                        .padding(.trailing,10)
-                        .sheet(isPresented: $showSettings) {
-                            SettingsView()
-                        }
-                    }
+                 }
                 }
                 .navigationTitle("Split!")
             }
